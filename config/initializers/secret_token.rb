381bf7f72775b4882cda0017cc79ba184155701e
astro <astro.monitor@gmail.com>
2012-11-22 16:52:07 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoAap::Application.config.secret_token = '6ed62bba66a0e437aeebc009880c458f8b8d73c982cf0dc3d60204719893fb0a74b6e7ede5ee3951f8aa316531ba7dc3dfd8bc94814b91026f64a3289b67b945'
